dcb38059c50720bfa84accaeaae1ef362137c6c6
Gerardo Barzola <gbarzolas@pucp.pe>
2017-12-20 01:06:44 +0000

    creacion del metodo text_field

--- app/form_builder/bootstrap_form_builder.rb
+++ app/form_builder/bootstrap_form_builder.rb
@@ -1,3 +1,8 @@
 class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
-    
+    delegate :content_tag, to: :@template
+    def text_field(name,*arg)
+        content_tag :div, class:"form-group" do
+            content_tag :label, name: name + super(name,*arg)
+        end
+    end
 end
