06f4a3687893e72dd6e7b4fa81d168b0306e998b
Gerardo Barzola <gbarzolas@pucp.pe>
2017-12-21 03:18:05 +0000

    mejoras, formularios y errores

--- app/form_builder/bootstrap_form_builder.rb
+++ app/form_builder/bootstrap_form_builder.rb
@@ -1,8 +1,86 @@
 class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
+    
     delegate :content_tag, to: :@template
-    def text_field(name,*arg)
+    
+    def number_field(name,*args)
+        options = args.extract_options!
+        if options.has_key?(:class)
+            options[:class] += "form-control"
+        else
+            options[:class] = "form-control"
+        end
         content_tag :div, class:"form-group" do
-            content_tag :label, name: name + super(name,*arg)
+            content_tag( :label, name , name: name ) + super(name,options)
         end
     end
+    
+    def email_field(name,*args)
+        options = args.extract_options!
+        if options.has_key?(:class)
+            options[:class] += "form-control"
+        else
+            options[:class] = "form-control"
+        end
+        content_tag :div, class:"form-group" do
+            content_tag( :label, name , name: name ) + super(name,options)
+        end
+    end
+    
+    def url_field(name,*args)
+        options = args.extract_options!
+        if options.has_key?(:class)
+            options[:class] += "form-control"
+        else
+            options[:class] = "form-control"
+        end
+        content_tag :div, class:"form-group" do
+            content_tag( :label, name , name: name ) + super(name,options)
+        end
+    end
+    
+    def text_field(name,*args)
+        options = args.extract_options!
+        if options.has_key?(:class)
+            options[:class] += "form-control"
+        else
+            options[:class] = "form-control"
+        end
+        content_tag :div, class:"form-group" do
+            content_tag( :label, name , name: name ) + super(name,options)
+        end
+    end
+    
+    def text_area(name,*args)
+        options = args.extract_options!
+        if options.has_key?(:class)
+            options[:class] += "form-control"
+        else
+            options[:class] = "form-control"
+        end
+        content_tag :div, class:"form-group" do
+            content_tag( :label, name , name: name ) + super(name,options)
+        end
+    end
+    
+    def submit(*args)
+        content_tag :div do
+            super(*args, class:"btn btn-success")
+        end
+    end
+    
+    def errors
+		if object.errors.any?
+			content_tag :div, class:"alert alert-danger large-padding white-text" do
+				content_tag(:h2, "Errores") +
+				content_tag(:ul) do
+					lis = ""
+					object.errors.full_messages.each do |error|
+						lis += content_tag :li, error
+					end
+					lis.html_safe
+				end
+			end
+		end
+	end
+    
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   belongs_to :user
-  validates_prescense_of :name, :user, :pricing
-  validtates :pricing, numericality: {greater_than: 0}
+  validates_presence_of :name, :user, :pricing
+  validates :pricing, numericality: {greater_than: 0}
   
 end
