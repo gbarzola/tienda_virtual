8cb50e9a8031b37dd825bc6f188abc0212398b97
Gerardo Barzola <gbarzolas@pucp.pe>
2017-12-25 15:01:18 +0000

    migracion y agregar attachment

--- db/migrate/20171225145927_create_attachments.rb
+++ db/migrate/20171225145927_create_attachments.rb
@@ -2,7 +2,8 @@ class CreateAttachments < ActiveRecord::Migration
   def change
     create_table :attachments do |t|
       t.references :product, index: true, foreign_key: true
-
+      t.attachment :avatar
+      
       t.timestamps null: false
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212162938) do
+ActiveRecord::Schema.define(version: 20171225145927) do
+
+  create_table "attachments", force: :cascade do |t|
+    t.integer  "product_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
+
+  add_index "attachments", ["product_id"], name: "index_attachments_on_product_id"
 
   create_table "my_emails", force: :cascade do |t|
     t.string   "email"
