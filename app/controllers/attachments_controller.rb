055f2f7cd0edea9081a6f8838f1cd5656d9e09b4
Gerardo Barzola <gbarzolas@pucp.pe>
2017-12-25 16:48:13 +0000

    agregar crear adjuntar archivo a producto

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,5 +1,9 @@
 class AttachmentsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_owner!
+  
   def create
+    raise params
   end
 
   def destroy
@@ -7,4 +11,14 @@ class AttachmentsController < ApplicationController
 
   def new
   end
+  
+  private
+    def authenticate_owner!
+      @product = Product.find(params[:product_id])
+      if @product.nil? || @product.user != current_user
+        redirect_to root_path , notice: "No puedes editar ese producto"
+        return
+      end
+    end
+  
 end
