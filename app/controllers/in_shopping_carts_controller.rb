0728abf3b583f72801e09456740b4d44c4bccddc
Gerardo Barzola <gbarzolas@pucp.pe>
2017-12-30 04:18:51 +0000

    creacion del controlador in shopping carts

---
+++ app/controllers/in_shopping_carts_controller.rb
@@ -0,0 +1,2 @@
+class InShoppingCartsController < ApplicationController
+end
---
+++ test/controllers/in_shopping_carts_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class InShoppingCartsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
