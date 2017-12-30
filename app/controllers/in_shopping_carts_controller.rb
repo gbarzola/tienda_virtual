da882dba03c601ebc5a50eac4a01a9a0cf11df35
Gerardo Barzola <gbarzolas@pucp.pe>
2017-12-30 04:27:33 +0000

    creacion de metodos de la clase shopping_carts_controller

--- app/controllers/in_shopping_carts_controller.rb
+++ app/controllers/in_shopping_carts_controller.rb
@@ -1,2 +1,11 @@
 class InShoppingCartsController < ApplicationController
+
+    def create
+        #agregar carrito de compra
+    end
+    
+    def destroy
+        #eliminar carrito de compra
+    end
+
 end
