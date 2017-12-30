a30fac62c5df87e9c8525fb40f6c41c4c81dda60
Gerardo Barzola <gbarzolas@pucp.pe>
2017-12-30 04:32:10 +0000

    añadir algoritmos de creacion y busqueda de la clase shopping_carts_controller

--- app/controllers/in_shopping_carts_controller.rb
+++ app/controllers/in_shopping_carts_controller.rb
@@ -2,10 +2,23 @@ class InShoppingCartsController < ApplicationController
 
     def create
         #agregar carrito de compra
+        in_shopping_cart = InShoppingCart.new(product_id: params[:product_id], shopping_cart: @shopping_cart)
+        
+        if in_shopping_cart.save
+            redirect_to "/carrito", notice: "Guardamos producto en carrito"
+        else
+            redirect_to products_path(id: params[:product_id]), notice: "No pudimos agregar al carrito"
+        end
+        
     end
     
     def destroy
         #eliminar carrito de compra
+        @in_shopping_cart = InShoppingCart.find(params[:id])
+
+		@in_shopping_cart.destroy
+
+		redirect_to carrito_path
     end
 
 end
