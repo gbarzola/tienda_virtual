20bf2539d29a7f10a5576748951fdeedc9d9e27b
Gerardo Barzola <gbarzolas@pucp.pe>
2017-12-31 17:13:53 +0000

    imagenes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   post "/emails/create", as: :create_email
+  post "/pagar/", to "payments#create"
   
   get "/carrito", to: "shopping_carts#show"
   get "/add/:product_id",as: :add_to_cart,to: "in_shopping_carts#create" 
