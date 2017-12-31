c1bc67fa54303ced5f621b14f9b837c58d3d0ad7
Gerardo Barzola <gbarzolas@pucp.pe>
2017-12-31 17:16:39 +0000

    creacion de controlador payment

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'payments/create'
+
   resources :attachments , only: [:create,:destroy,:new,:show]
   resources :products
   resources :in_shopping_carts , only:[:create,:destroy]
   devise_for :users
   
   post "/emails/create", as: :create_email
-  post "/pagar/", to "payments#create"
+
   
   get "/carrito", to: "shopping_carts#show"
   get "/add/:product_id",as: :add_to_cart,to: "in_shopping_carts#create" 
