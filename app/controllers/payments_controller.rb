506cbcabf496fdbb1fa56724709d6b2002b0b42b
Gerardo Barzola <gbarzolas@pucp.pe>
2017-12-31 17:27:31 +0000

    modificacion de la funcion create del controlador payments controler

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,26 @@
 class PaymentsController < ApplicationController
   def create
+    @payment = Payment.new({
+			intent: "sale",
+			payer:{
+				payment_method: "paypal"
+			},
+			transactions: [
+				{
+					item_list: {
+						items: "DEMO"
+					},
+					amount:{
+						total: (@shopping_cart.total / 100),
+						currency: "USD"
+					},
+					description: "Compra de tus productos en nuestra plataforma"
+				}
+			],
+			redirect_urls: {
+				return_url: @return_url,
+				cancel_url: @cancel_url
+			}
+    })
   end
 end
