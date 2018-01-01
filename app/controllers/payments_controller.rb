31b0b5537d2dfb0102dd74ab74c60b6cbf07d83f
Gerardo Barzola <gbarzolas@pucp.pe>
2018-01-01 20:36:00 +0000

    Mejora del proceso de pago hasta realizar el checkout

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,6 +2,23 @@ class PaymentsController < ApplicationController
   
   include PayPal::SDK::REST
   
+  def checkout
+  	@my_payment = MyPayment.find_by(paypal_id: params[:paymentId])
+  	if @my_payment.nil?
+  		redirect_to "/carrito"
+  	else
+  		
+  		payment = Payment.find(@my_payment.paypal_id)
+  		
+  		if payment.execute(payer_id: params[:PayerID])
+  			redirect_to carrito_path, notice:"Se proceso pago"
+  		else
+  			redirect_to carrito_path, notice:"Hubo algun error en el pago"
+  		end
+  	end
+  		
+  end
+  
   def create
     payment = Payment.new({
 			intent: "sale",
@@ -21,16 +38,19 @@ class PaymentsController < ApplicationController
 				}
 			],
 			redirect_urls: {
-				return_url: "https://hackathon-minsa-gbarzola1.c9users.io",
+				return_url: "https://hackathon-minsa-gbarzola1.c9users.io/checkout",
 				cancel_url: "https://hackathon-minsa-gbarzola1.c9users.io/carrito"
 			}
     })
     
     if payment.create
-			 @payment = MyPayment.new(paypal_id: payment.id , ip: request.remote_ip , shopping_cart_id: cookies[:shopping_cart_id] )
+			 
+			 @my_payment = MyPayment.create!(paypal_id: payment.id , ip: request.remote_ip , shopping_cart_id: cookies[:shopping_cart_id] )
+			 
 			 redirect_to payment.links.find{|v| v.method == "REDIRECT" }.href
+			 
 		else
-			raise payment.errors.to_yaml
+			raise payment.error.to_yaml
 		end
     
   end
